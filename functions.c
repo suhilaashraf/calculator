d2431923dbdc72836353ca5db8e29f5ea18e7efd
suhilaashraf <85295518+suhilaashraf@users.noreply.github.com>
2023-11-18 00:04:29 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,9 +6,6 @@ int result = dividend % divisor ;
 
  return result ;
 }
-
-
-
 // sum two numbers 
 
 float sum(float num1 , float num2)
@@ -19,19 +16,18 @@ float sum(float num1 , float num2)
 //function for multiplication with floating-point numbers
 float multiply(float num1, float num2) {
     return num1 * num2;
+}
 
 float subtract(float num1, float num2)
 {
     return num1-num2;
 }
 
-
 //the divide function
 float divide(float n1,float n2)
 
 {
 	return n1/n2;
-
 }
 
 
